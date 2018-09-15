9ca2659cf54228a0a1d8f6be0d41a563077a2608
Christopher McDonald <mail@christophermcdonald.me>
2018-09-15 09:33:09 -0700

    stuff

--- NNClient.py
+++ NNClient.py
@@ -4,10 +4,42 @@ from keras.wrappers.scikit_learn import KerasClassifier
 from sklearn.model_selection import GridSearchCV
 import NeuralNet
 
-NeuralNet.NeuralNet()
-# algo, LR, Momentum, Layers + Nodes, Dropout, Batch
-# Algo:
-# RMS prop - 0.5906993576404596
-#
-# LR:
-# 0.001 - 0.5780458383594692
+# NeuralNet.NeuralNet()
+
+# from dataImport import getData
+X, Y = getData(False)
+
+# Split Data into Testing & Training (20/80)
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size = 0.20, random_state = 0)
+indexes = y_train[y_train == 0].sample(frac = 0.75).index
+y_train = y_train.drop(indexes)
+X_train = X_train.drop(indexes)
+
+optimizers = ['adam', 'rmsprop', 'sgd', 'adadelta', 'adagrad', 'adamax', 'nadam'];
+batch_size = [8, 16, 32, 64, 128, 256]
+epochs = [10, 50, 100]
+learn_rate = [0.001, 0.01, 0.1, 0.2, 0.3]
+momentum = [0.0, 0.2, 0.4, 0.6, 0.8, 0.9]
+param_grid = dict(batch_size = batch_size, epochs = epochs, optimizers = optimizers, learn_rate = learn_rate, momentum = momentum)
+
+# import relevant libraries, use GridSearchCV to find optimal values
+from sklearn.model_selection import GridSearchCV
+from keras.wrappers.scikit_learn import KerasClassifier
+model = KerasClassifier(build_fn=NeuralNet.NeuralNet(), verbose=0)
+clf = GridSearchCV(model, param_grid, scoring = 'accuracy', n_jobs = -1)
+clf.fit(X_train, y_train)
+
+print("Best parameters set found on development set:")
+print()
+print(clf.best_params_)
+print("Grid scores on development set:")
+print()
+means = clf.cv_results_['mean_test_score']
+stds = clf.cv_results_['std_test_score']
+for mean, std, params in zip(means, stds, clf.cv_results_['params']):
+    print("%0.3f (+/-%0.03f) for %r"
+          % (mean, std * 2, params))
+print()
+
+# Algo, LR, Momentum, Layers + Nodes, Dropout, Batch
--- RandomForest.py
+++ RandomForest.py
@@ -6,45 +6,40 @@ X, Y = getComps(3, False)
 from sklearn.cross_validation import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size = 0.20, random_state = 0)
 
-crits = ['gini', 'entropy']
-maxes = ['log2', 'auto', 0.2, 0.4, 0.6, 0.8, 1.0]
-trees = [2, 4, 8, 16, 32, 64, 128, 256, 512, 1024]
-
-params = {'criterion': crits, 'max_features': maxes, 'n_estimators': trees};
-
-# import relevant libraries, use GridSearchCV to find optimal values
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.model_selection import GridSearchCV
-clf = GridSearchCV(RandomForestClassifier(class_weight = {1: 4, 0: 1}, random_state = 0), params, scoring = 'accuracy', n_jobs = -1)
-clf.fit(X_train, y_train)
-
-out = open("output.txt", "w");
-
-out.write("Best parameters set found on development set:")
-out.write()
-out.write(clf.best_params_)
-out.write("Grid scores on development set:")
-out.write()
-means = clf.cv_results_['mean_test_score']
-stds = clf.cv_results_['std_test_score']
-for mean, std, params in zip(means, stds, clf.cv_results_['params']):
-    out.write("%0.3f (+/-%0.03f) for %r"
-          % (mean, std * 2, params))
-out.write()
-out.close();
-
-# find best params from above and plug into below
+# crits = ['entropy']
+# maxes = ['auto']
+# trees = [1024]
+#
+# params = {'criterion': crits, 'max_features': maxes, 'n_estimators': trees};
+#
+# # import relevant libraries, use GridSearchCV to find optimal values
+# from sklearn.ensemble import RandomForestClassifier
+# from sklearn.model_selection import GridSearchCV
+# clf = GridSearchCV(RandomForestClassifier(class_weight = {1: 4, 0: 1}, random_state = 0), params, scoring = 'accuracy', n_jobs = -1)
+# clf.fit(X_train, y_train)
+#
+# print("Best parameters set found on development set:")
+# print()
+# print(clf.best_params_)
+# print("Grid scores on development set:")
+# print()
+# means = clf.cv_results_['mean_test_score']
+# stds = clf.cv_results_['std_test_score']
+# for mean, std, params in zip(means, stds, clf.cv_results_['params']):
+#     print("%0.3f (+/-%0.03f) for %r"
+#           % (mean, std * 2, params))
+# print()
 
 # Fitting Kernel SVM to the Training set
-# from sklearn.ensemble import RandomForestClassifier
-# classifier = RandomForestClassifier(n_estimators = 5, criterion = 'gini', random_state = 0, class_weight={1: 4, 0: 1}, max_features = val2)
-# classifier.fit(X_train, y_train)
-#
-# # Predicting the Test set results
-# y_pred = classifier.predict(X_test)
-#
-# # Making the Confusion Matrix
-# from sklearn.metrics import confusion_matrix
-# from sklearn.metrics import accuracy_score
-# cm = confusion_matrix(y_test, y_pred)
-# acc = accuracy_score(y_test, y_pred)
+from sklearn.ensemble import RandomForestClassifier
+classifier = RandomForestClassifier(n_estimators = 1024, criterion = 'entropy', random_state = 0, class_weight={1: 4, 0: 1}, max_features = 'auto')
+classifier.fit(X_train, y_train)
+
+# Predicting the Test set results
+y_pred = classifier.predict(X_test)
+
+# Making the Confusion Matrix
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score
+cm = confusion_matrix(y_test, y_pred)
+acc = accuracy_score(y_test, y_pred)
